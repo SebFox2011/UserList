eb805e5b6b9c525bf570cc7b7bde65a7ea8845f2
SebFox2011 <sebastien.philippe5@gmail.com>
2019-08-19 16:18:55 +0200

    Ajout du reducer reducer_users et du fichier index

---
+++ src/Reducers/reducer_users.js
@@ -0,0 +1,9 @@
+export default function f() {
+    return [
+        {nom: 'Roman PHILIPPE'},
+        {nom: 'Sébastien PHILIPPE'},
+        {nom: 'Claire MARTIN'},
+        {nom: 'Jonathan MABILLAIS'},
+        {nom: 'Antoine COCHE'}
+    ]
+}
